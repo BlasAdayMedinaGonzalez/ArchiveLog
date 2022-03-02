8be53c7bd108591a9df6fe4f11ee05c6ee7768c9
BlasAdayMedinaGonzalez <usr2702@salesianos-lacuesta.net>
2022-03-02 00:49:29 +0000

    Feature: Added ssh lib, added connection questions

--- src/com/adaysoft/Main.java
+++ src/com/adaysoft/Main.java
@@ -1,8 +1,50 @@
 package com.adaysoft;
 
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        Session session = null;
+
+        ChannelExec channel = null;
+
+        try {
+            System.out.println("Enter host ip:");
+            String hostIp = sc.nextLine();
+            System.out.println("Enter port: ");
+            int port = sc.nextInt();
+
+            System.out.println("Enter username:");
+            String username = sc.nextLine();
+
+            System.out.println("Enter password:");
+            String password = sc.nextLine();
+            session.setPassword(password);
+
+            session = new JSch().getSession(username,hostIp,port);
+
+
+            session.connect();
+            System.out.println("Connected...");
+
+
+
+
+
+        } catch (JSchException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) session.disconnect();
+            if (channel != null) channel.disconnect();
+            if (sc != null) sc.close();
+        }
     }
 }
